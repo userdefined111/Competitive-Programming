9e8077f22ec92e1750156a048c8943dbc92b7b58
userdefined111 <kapilsoni1883@gmail.com>
2026-05-19 23:57:36 +0530

    19/05/2026

--- 800-Rated-Problems/Goals Of Victory.cpp
+++ 800-Rated-Problems/Goals Of Victory.cpp
@@ -5,6 +5,15 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+           cin>>a[i];
+           sum+=a[i];
+        }
+        cout<<-1*sum<<endl;
     }
+    return 0;
 }
