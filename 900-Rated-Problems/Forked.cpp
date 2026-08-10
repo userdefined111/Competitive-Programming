d93f22ac122e88aa2c1596a3dcb884381e2ee6b7
kapil soni <kapilsoni1883@gmail.com>
2026-08-10 06:27:14 +0530

    10/08/2026

--- 900-Rated-Problems/Forked.cpp
+++ 900-Rated-Problems/Forked.cpp
@@ -1,5 +1,7 @@
-#include <bits\stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int dx[4]={-1,1,-1,1}, dy[4]={-1,-1,1,1};
 
 int main (){
     int t;
@@ -7,13 +9,26 @@ int main (){
     while(t--){
         //Inputs
         long long a,b;
-        cin>>a>>b>>endl;
+        cin>>a>>b;
         long long xk,yk;
-        cin>>xk>>yk>>endl;
+        cin>>xk>>yk;
         long long xq,yq;
-        cin>>xq>>yq>>endl;
+        cin>>xq>>yq;
         
         //Logic
-        
+        set<pair<int,int>>kh,qh;
+        for(int i=0;i<4;i++){
+            kh.insert({xk+ dx[i]*a, yk+dy[i]*b});
+            kh.insert({xk+ dx[i]*b, yk+dy[i]*a});
+            qh.insert({xq+ dx[i]*a, yq+dy[i]*b});
+            qh.insert({xq+ dx[i]*b, yq+dy[i]*a});
+        }
+        long long ans=0;
+        for(auto pos:kh){
+            if(qh.find(pos)!= qh.end())
+            ans++;
+        }
+        cout<<ans<<endl;
     }
+    return 0;
 }
